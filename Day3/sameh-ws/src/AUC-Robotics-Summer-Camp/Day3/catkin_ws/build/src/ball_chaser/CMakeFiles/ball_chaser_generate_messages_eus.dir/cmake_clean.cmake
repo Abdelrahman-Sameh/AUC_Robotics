3943d11502c9486eea896ef1831b181ecb45c2ab
Abdelrahman-Sameh <abdelrahman.sameh.111@gmail.com>
2020-08-14 17:17:40 +0200

    Task4 for Day3: Cloning a package, Making executable package inside Day3 Directory and Fixing Service ERRORS

---
+++ Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build/src/ball_chaser/CMakeFiles/ball_chaser_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ball_chaser_generate_messages_eus"
+  "/home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel/share/roseus/ros/ball_chaser/srv/DriveToTarget.l"
+  "/home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel/share/roseus/ros/ball_chaser/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ball_chaser_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build/src/ball_chaser/CMakeFiles/process_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process_image.dir/src/process_image.cpp.o"
+  "/home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel/lib/ball_chaser/process_image.pdb"
+  "/home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/src/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel/lib/ball_chaser/process_image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
