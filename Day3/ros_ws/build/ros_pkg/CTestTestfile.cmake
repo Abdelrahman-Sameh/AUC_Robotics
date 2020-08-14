4fe4f3b2da2f2d2eeee24d7687426ab25d94161f
Abdelrahman-Sameh <abdelrahman.sameh.111@gmail.com>
2020-08-14 14:50:27 +0200

    Updating Tasks after cloning package and removing devel and build

--- Day3/ros_ws/build/ros_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ros_ws/src/ros_pkg
-# Build directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ros_ws/build/ros_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ Day3/sameh-ws/build/pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/src/pkg
+# Build directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/sameh-ws/build/pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
