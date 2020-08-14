a177d6c36c61fdc7e3c40303e95b57e7d149d165
Abdelrahman-Sameh <abdelrahman.sameh.111@gmail.com>
2020-08-14 04:23:34 +0200

    Task2 & Task3 Repeated with New Workspace because of some errors

--- Day3/ROS_ws/build/ROS_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ROS_ws/src/ROS_pkg
-# Build directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ROS_ws/build/ROS_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ Day3/ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ros_ws/src
+# Build directory: /home/abdelrahman/Desktop/AUC_Robotics/Day3/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_pkg")
